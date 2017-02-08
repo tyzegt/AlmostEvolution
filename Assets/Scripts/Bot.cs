2d08ac3720c90b494b867d833b2b96a517f0b6f5
Tyzegt <admin@almostgames.ru>
2017-02-08 19:08:09 +0600

    Prototype

---
+++ Assets/Scripts/Bot.cs
@@ -0,0 +1,386 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bot : MonoBehaviour {
+
+    public Transform[] sensors;
+    private Transform sensor;
+
+    public LayerMask side;
+    public LayerMask wall;
+    public LayerMask food;
+    public LayerMask bot;
+    public Collider2D[] colliders;
+    [Space()]
+    public int[] genome;
+    public int energy;
+    [Space()]
+    public int controller;
+    public int steps;
+    [Space()]
+    public float red;
+    public float green;
+    public float blue;
+    private float rot;
+
+    public GameObject deadCell;
+
+    // Use this for initialization
+    void Start () {
+        //genome = new int[64];
+        controller = 0;
+
+        if(LevelManager.startEnergy > 0) energy = LevelManager.startEnergy;
+        if (Random.Range(0, 3) == 1)
+        {
+            FindObjectOfType<LevelManager>().mutations++;
+            genome[Random.Range(0, 63)] = Random.Range(0, 63);
+        }
+
+        ChangeColor();
+
+        for(int i = 0; i< genome.Length; i++)
+        {
+            //genome[i] = Random.Range(0,63);
+            //genome[i] = 10;
+        }
+
+        sensor = sensors[0];
+
+    }
+	
+	public void ChangeColor()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(red, green, blue);
+    }
+
+    public void Step()
+    {
+        colliders = Physics2D.OverlapCircleAll(sensor.position, 0.1f);
+        if (controller > 63) controller -= 64;
+
+        energy--;
+        GetComponent<SpriteRenderer>().color = new Color(red, green, blue);
+
+        if (energy > LevelManager.energyToDivide)
+        {
+            TryToDivide();
+        }
+        if (energy <= 0) {
+            Destroy(gameObject);
+            //Debug.Log("starved");
+        }
+
+        // смотрит
+        if (genome[controller] == 0)
+        {
+            Look();
+            return;
+        }
+        // поворачивается
+        else if(genome[controller] > 0 && genome[controller] < 8)
+        {
+            Turn();
+            return;
+        }
+        // жрёт
+        else if (genome[controller] == 8)
+        {
+            Eat();
+            return;
+        }
+        // жрёт
+        else if (genome[controller] == 9)
+        {
+            Move();
+            return;
+        }
+        // фотосинтез
+        else if (genome[controller] == 10)
+        {
+            Synth();
+            return;
+        }
+        // проверяет здоровье
+        else if (genome[controller] == 11)
+        {
+            CheckEnergy();
+            return;
+        }
+        // рожает
+        /*
+        else if (genome[controller] == 12)
+        {
+            TryToDivide();
+            return;
+        }
+        */
+        // переход
+        else if (genome[controller] > 11)
+        {
+            controller += genome[controller];
+            steps++;
+            return;
+        }
+        
+    }
+
+    void TryToDivide()
+    {
+        for(int i = sensors.Length-1; i >= 0 ; i--)
+        {
+            if (Physics2D.OverlapCircleAll(sensors[i].position, 0.1f).Length == 0)
+            {
+                Divide(sensors[i]);
+                return;
+            }
+        }
+        Die();  
+    }
+
+    void Divide(Transform tr)
+    {
+        
+        Instantiate(this, new Vector2(Mathf.Round(tr.position.x), Mathf.Round(tr.position.y)), tr.rotation);
+        energy /= 2;
+        steps++;
+    }
+
+    void Look()
+    {
+        if (colliders.Length == 0)                                                  // Пусто
+        {
+            controller++;
+            steps++;
+            return;
+        }
+        
+        if (colliders[0].gameObject.layer == LayerMask.NameToLayer("side"))         // Край
+        {
+            controller++;
+            steps++;
+            return;
+        }
+        else if (colliders[0].gameObject.layer == LayerMask.NameToLayer("wall"))    // стена
+        {
+            controller += 2;
+            steps++;
+            return;
+        }
+        else if (colliders[0].gameObject.layer == LayerMask.NameToLayer("food"))    // еда
+        {
+            controller += 3;
+            steps++;
+            return;
+        }
+        else if (colliders[0].gameObject.layer == LayerMask.NameToLayer("bot"))     // бот
+        {
+            if (CheckRelations()) controller += 4;
+            else controller += 5;
+            steps++;
+            return;
+        }
+        
+    }
+
+    void Turn()
+    {
+        rot += 45 * genome[controller];
+        if (rot > 360) rot -= 360;
+        transform.rotation = Quaternion.Euler(0, 0, rot);
+        controller++;
+        steps++;
+    }
+
+    void Eat()
+    {
+        if (colliders.Length == 0)                                                  // Пусто
+        {
+            controller++;
+            steps = 15;
+            return;
+        }
+
+        if (colliders[0].gameObject.layer == LayerMask.NameToLayer("side"))         // Край
+        {
+            controller++;
+            steps = 15;
+            return;
+        }
+        else if (colliders[0].gameObject.layer == LayerMask.NameToLayer("wall"))    // стена
+        {
+            controller += 2;
+            steps = 15;
+            return;
+        }
+        else if (colliders[0].gameObject.layer == LayerMask.NameToLayer("food"))    // еда
+        {
+            energy += LevelManager.callories;
+            controller += 3;
+            steps = 15;
+            Destroy(colliders[0].gameObject);
+
+            red += 1f;
+            if (red > 1) red = 1;
+            green -= 0.1f;
+            if (green < 0) green = 0;
+            blue -= 0.1f;
+            if (blue < 0) blue = 0;
+
+            return;
+        }
+        else if (colliders[0].gameObject.layer == LayerMask.NameToLayer("bot"))     // бот
+        {
+            if (CheckRelations())
+            {
+                controller += 4;
+                energy += LevelManager.callories;
+            }
+            else
+            {
+                controller += 5;
+                energy += LevelManager.callories;
+            }
+            Destroy(colliders[0].gameObject);
+            steps=15;
+
+            red += 1f;
+            if (red > 1) red = 1;
+            green -= 0.1f;
+            if (green < 0) green = 0;
+            blue -= 0.1f;
+            if (blue < 0) blue = 0;
+
+            return;
+        }
+
+    }
+
+    void Move()
+    {
+        if (colliders.Length == 0)                                                  // Пусто
+        {
+            transform.position = new Vector2(Mathf.Round(sensor.position.x), Mathf.Round(sensor.position.y));
+            controller++;
+            steps = 15;
+            return;
+        }
+
+        if (colliders[0].gameObject.layer == LayerMask.NameToLayer("side"))         // Край
+        {
+            float newx = Mathf.Round(sensor.position.x);
+            if (newx < 0) newx = 99;
+            if (newx > 99) newx = 0;
+            transform.position = new Vector2(newx, Mathf.Round(sensor.position.y));
+            controller++;
+            steps = 15;
+            return;
+        }
+        else if (colliders[0].gameObject.layer == LayerMask.NameToLayer("wall"))    // стена
+        {
+            controller += 2;
+            steps = 15;
+            return;
+        }
+        else if (colliders[0].gameObject.layer == LayerMask.NameToLayer("food"))    // еда
+        {
+            energy += LevelManager.callories;
+            controller += 3;
+            steps = 15;
+            Destroy(colliders[0].gameObject);
+
+            transform.position = new Vector2(Mathf.Round(sensor.position.x), Mathf.Round(sensor.position.y));
+
+            red += 1f;
+            if (red > 1) red = 1;
+            green -= 0.1f;
+            if (green < 0) green = 0;
+            blue -= 0.1f;
+            if (blue < 0) blue = 0;
+        }
+        else if (colliders[0].gameObject.layer == LayerMask.NameToLayer("bot"))     // бот
+        {
+            if (CheckRelations())
+            {
+                controller += 4;
+                energy += LevelManager.callories;
+            }
+            else
+            {
+                controller += 5;
+                energy += LevelManager.callories;
+            }
+            Destroy(colliders[0].gameObject);
+            steps = 15;
+
+            transform.position = new Vector2(Mathf.Round(sensor.position.x), Mathf.Round(sensor.position.y));
+
+            red += 1f;
+            if (red > 1) red = 1;
+            green -= 0.1f;
+            if (green < 0) green = 0;
+            blue -= 0.1f;
+            if (blue < 0) blue = 0;
+
+            return;
+        }
+
+    }
+
+    void Synth()
+    {
+        energy += (int)Mathf.Round(transform.position.y * LevelManager.synthMultipler);
+        controller++;
+        steps = 15;
+
+        
+        green += 0.1f;
+        if (green > 1) green = 1;
+        blue -= 0.1f;
+        if (blue < 0) blue = 0;
+        red -= 0.1f;
+        if (red < 0) red = 0;
+    }
+
+    void CheckEnergy()
+    {
+        int cnt = controller + 1;
+        if (cnt > 63) cnt -= 64;
+        if (energy < genome[cnt]*15)
+        {
+            controller += 2;
+            steps++;
+        }
+        if (energy >= genome[cnt]*15)
+        {
+            controller += 3;
+            steps++;
+        }
+    }
+
+    public void Die()
+    {
+        Instantiate(deadCell, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    bool CheckRelations()
+    {
+        int dismatches = 0;
+
+        for (int i = 0; i < genome.Length; i++)
+        {
+            if (dismatches < 2)
+            {
+                if (genome[i] != colliders[0].GetComponent<Bot>().genome[i]) dismatches++;
+            }
+            else return false;
+        }
+        return true;
+    }
+
+    void Update()
+    {
+        Step();
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public int calloriesValue;
+    public static int callories;
+        
+    public int startEnergyValue;
+    public static int startEnergy;
+
+    public float synthMultiplerValue;
+    public static float synthMultipler;
+
+    public int energyToDivideValue;
+    public static int energyToDivide;
+
+    GameObject newCell;
+
+    public int mutations;
+    public TextMesh mutationsText;
+    public TextMesh fpsText;
+
+
+    public GameObject cell;
+
+    Bot[] cells;
+
+    // Use this for initialization
+    void Start () {
+        callories = calloriesValue;
+        startEnergy = startEnergyValue;
+        synthMultipler = synthMultiplerValue;
+        energyToDivide = energyToDivideValue;
+        Application.runInBackground = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        cells = FindObjectsOfType<Bot>();
+        if (cells.Length == 0)
+        {
+            GameObject[] corpses = GameObject.FindGameObjectsWithTag("food");
+            for (int i = 0; i < corpses.Length; i++)
+            {
+                Destroy(corpses[i]);
+            }
+            Instantiate(cell, new Vector2(48, 56), transform.rotation);
+        }
+        //Debug.Log(Time.deltaTime);
+
+        mutationsText.text = mutations.ToString();
+        fpsText.text = (1 / Time.deltaTime).ToString();
+    }
+   
+}
