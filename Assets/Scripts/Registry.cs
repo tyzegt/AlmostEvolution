f13edff3c73b867b75fc31d8d57954ff4218d6d1
Ivan Gusarov <xopuct@gmail.com>
2017-02-10 02:21:50 +0300

    Registry Improvements

--- Assets/Scripts/Registry.cs
+++ Assets/Scripts/Registry.cs
@@ -8,17 +8,23 @@ public class Registry : Singleton<Registry>
     Dictionary<int, GameObject> Cells = new Dictionary<int, GameObject>();
     Dictionary<int, GameObject> Corpses = new Dictionary<int, GameObject>();
 
-    Vector2 ValidatePos(Vector3 position)
+    //HashSet<GameObject> Cells = new HashSet<GameObject>();
+    //HashSet<GameObject> Corpses = new HashSet<GameObject>();
+
+    Vector3 ValidatePos(Vector3 position)
     {
         return new Vector2(Mathf.Round(position.x), Mathf.Round(position.y));
     }
 
     public GameObject Add(Vector3 position, Quaternion rotation, Bot prefab)
     {
-        var inst = (Bot)Instantiate(prefab, ValidatePos(position), rotation);
+        //var inst = GameObjectPool.Instance.Instantiate(prefab.gameObject, ValidatePos(position), rotation);
+        //inst.GetComponent<Bot>().Copy(prefab);
+        var inst = (GameObject)Instantiate(prefab.gameObject, ValidatePos(position), rotation);
 
-        Cells.Add(inst.gameObject.GetInstanceID(), inst.gameObject);
-        return inst.gameObject;
+
+        Cells[inst.GetInstanceID()] = inst;
+        return inst;
     }
 
     public void Kill(GameObject deadmen)
@@ -33,19 +39,21 @@ public class Registry : Singleton<Registry>
 
     public void MakeCorpse(Vector2 position, Quaternion rotation)
     {
+        //var corpse = GameObjectPool.Instance.Instantiate(DeadCell, ValidatePos(position), rotation);
         var corpse = (GameObject)Instantiate(DeadCell, ValidatePos(position), rotation);
-        Corpses.Add(corpse.GetInstanceID(), corpse);
+
+        Corpses[corpse.GetInstanceID()] = corpse;
     }
 
     public void Remove(GameObject bot)
     {
         if (bot)
         {
-            if (IsCell(bot))
-                Cells.Remove(bot.GetInstanceID());
-            else if (IsCorpse(bot))
-                Corpses.Remove(bot.GetInstanceID());
+            Cells.Remove(bot.GetInstanceID());
+            Corpses.Remove(bot.GetInstanceID());
+
             Destroy(bot.gameObject);
+            //GameObjectPool.Instance.Destroy(bot.gameObject);
         }
     }
     public int GetCorpsesCount()
@@ -76,24 +84,24 @@ public class Registry : Singleton<Registry>
         return Cells.ContainsKey(obj.GetInstanceID());
     }
 
-    List<GameObject> GetInRadius(Vector3 position, float radius, Dictionary<int, GameObject> dict)
-    {
-        var ret = new List<GameObject>();
+    //List<GameObject> GetInRadius(Vector3 position, float radius, Dictionary<int, GameObject> dict)
+    //{
+    //    var ret = new List<GameObject>();
 
-        var sqrRadius = radius * radius;
-        foreach (var obj in dict)
-        {
-            if ((obj.Value.transform.position - position).sqrMagnitude < sqrRadius)
-                ret.Add(obj.Value);
+    //    var sqrRadius = radius * radius;
+    //    foreach (var obj in dict)
+    //    {
+    //        if ((obj.Value.transform.position - position).sqrMagnitude < sqrRadius)
+    //            ret.Add(obj.Value);
 
-        }
-        return ret;
-    }
+    //    }
+    //    return ret;
+    //}
 
-    public List<GameObject> GetInRadius(Vector3 position, float radius)
-    {
-        var ret = GetInRadius(position, radius, Cells);
-        ret.AddRange(GetInRadius(position, radius, Corpses));
-        return ret;
-    }
+    //public List<GameObject> GetInRadius(Vector3 position, float radius)
+    //{
+    //    var ret = GetInRadius(position, radius, Cells);
+    //    ret.AddRange(GetInRadius(position, radius, Corpses));
+    //    return ret;
+    //}
 }
